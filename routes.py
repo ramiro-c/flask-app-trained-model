9347ad0f54786cd6a0216330bb1f426319581f07
Ramiro Cerdá <rami992009@gmail.com>
2019-11-30 21:03:05 -0300

    separate app from routes

---
+++ routes.py
@@ -0,0 +1,28 @@
+from flask import render_template, request, url_for, redirect
+import app from app
+import joblib
+from sklearn.datasets import load_iris
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/prediction',methods=['POST'])
+def prediction():
+    sepal_length = request.form.get('sepal_length')
+    sepal_width = request.form.get('sepal_width')
+    sepal_length = request.form.get('petal_length')
+    sepal_width = request.form.get('petal_width')
+    
+    m = [sepal_length, sepal_width, sepal_length, sepal_width]
+
+    measures = [float(measure) for measure in m]
+    classifier = joblib.load('trained_model.pkl')
+    prediction = classifier.predict([measures])
+
+    iris = load_iris()
+    # Possible output -> 'setosa' (0), 'versicolor' (1) or 'virginica' (2)
+    kind_of_iris = iris['target_names'][prediction][0]
+
+    return render_template('prediction.html',kind_of_iris=kind_of_iris)
